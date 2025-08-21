56d1051e9e0ce910768387439cc6c493d95820cd
Edgar Agno <edgardo.agno@gmail.com>
2025-08-21 14:14:15 +0100

    Initial Commit

---
+++ TournamentSchedulerTests/TournamentSchedulerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TournamentSchedulerTests.swift
+//  TournamentSchedulerTests
+//
+//  Created by EDGARDO AGNO on 21/08/2025.
+//
+
+import Testing
+@testable import TournamentScheduler
+
+struct TournamentSchedulerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
