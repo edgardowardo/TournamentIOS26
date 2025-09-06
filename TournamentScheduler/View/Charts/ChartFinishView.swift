de89d6bb033abbd44ee2e99fa1f50fd2de5f4780
Edgar Agno <edgardo76@yahoo.com>
2025-09-06 12:47:24 +0100

    Preview

--- TournamentScheduler/View/Charts/ChartFinishView.swift
+++ TournamentScheduler/View/Charts/ChartFinishView.swift
@@ -30,8 +30,9 @@ struct ChartFinishView: View {
                 .foregroundStyle(by: .value("Type", dataItem.type))
                 .opacity(dataItem.isAnimated ? 1 : 0)
         }
+        .chartLegend(alignment: .center)
         .chartYScale(domain: 0...maxAmount)
-        .frame(height: 200)
+        .frame(width: 250, height: 250)
         .onAppear(perform: animateChart)
         .chartBackground { p in
             GeometryReader { g in
@@ -65,3 +66,32 @@ struct ChartFinishView: View {
         }
     }
 }
+
+
+#Preview {
+    struct PreviewableChartFinishView: View {
+        struct ViewModelProvider: StatisticsProviding {
+            var ranks: [RankInfo] {
+                [
+                    .init(oldrank: 2, rank: 1, name: "Alice", countParticipated: 5, countPlayed: 5, countWins: 4, countLost: 1, countDrawn: 0, pointsFor: 3, pointsAgainst: 0, pointsDifference: 5),
+                    .init(oldrank: 1, rank: 2, name: "Bob", countParticipated: 5, countPlayed: 5, countWins: 3, countLost: 1, countDrawn: 1, pointsFor: 0, pointsAgainst: 0, pointsDifference: 3),
+                    .init(oldrank: 3, rank: 3, name: "Carol", countParticipated: 5, countPlayed: 5, countWins: 2, countLost: 2, countDrawn: 1, pointsFor: 0, pointsAgainst: 0, pointsDifference: 2),
+                    .init(oldrank: 4, rank: 4, name: "Dave", countParticipated: 5, countPlayed: 5, countWins: 1, countLost: 3, countDrawn: 1, pointsFor: 0, pointsAgainst: 0, pointsDifference: 1),
+                    .init(oldrank: 5, rank: 5, name: "Eve", countParticipated: 5, countPlayed: 5, countWins: 0, countLost: 4, countDrawn: 1, pointsFor: 0, pointsAgainst: 0, pointsDifference: 0)
+                ]
+            }
+            let schedule: Schedule = .roundRobin
+            let nOverP: Int = 5
+            var countMatchWins: Int = 31
+            var countMatchDraws: Int = 4
+            var countMatches: Int = 16
+        }
+        var body: some View {
+            NavigationStack {
+                ChartFinishView(vm: ViewModelProvider())
+            }
+        }
+    }
+    return PreviewableChartFinishView()
+}
+
