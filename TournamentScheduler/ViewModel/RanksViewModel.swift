7c212b361439acf968f30e47779134c7e8de454e
Edgar Agno <edgardo76@yahoo.com>
2025-09-07 11:53:14 +0100

    RankInfo file

--- TournamentScheduler/ViewModel/RanksViewModel.swift
+++ TournamentScheduler/ViewModel/RanksViewModel.swift
@@ -1,26 +1,4 @@
 import Foundation
-
-struct RankInfo: Identifiable {
-    let id: UUID = .init()
-    var oldrank : Int
-    var rank : Int
-    var name : String
-
-    /// Used in Round Robin and American Doubles for N/P header showing how many games expecting to play excluding byes.
-    var countParticipated: Int
-
-    /// Played means Match has been played and finished where it's not a bye and is a draw or a winner announced
-    var countPlayed : Int
-    
-    /// Actual wins excluding byes
-    var countWins : Int
-    var countLost : Int
-    var countDrawn : Int
-    var countBye: Int
-    var pointsFor : Int
-    var pointsAgainst : Int
-    var pointsDifference : Int
-}
     
 struct RanksViewModel: StatisticsProviding {
     let ranks: [RankInfo]
